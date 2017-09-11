9f8d01821d32d148e5396f59b8337065ce4c8b55
Aryan Soni <aryansoni1108@gmail.com>
2017-09-12 01:00:15 +0530

    Flashlight turning On on shaking

---
+++ app/src/main/java/com/jetslice/shakeflashlightg4/MainActivity.java
@@ -0,0 +1,129 @@
+package com.jetslice.shakeflashlightg4;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.security.Policy;
+
+public class MainActivity extends AppCompatActivity  {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onShake(int count) {
+				/*
+				 * The following method, "handleShakeEvent(count):" is a stub //
+				 * method you would use to setup whatever you want done once the
+				 * device has been shook.**/
+
+
+                    flashLightOff(4);
+
+                try {
+                    flashlighton(2);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+
+
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+    private void handleshakeevent(int co){
+        Toast.makeText(MainActivity.this,"Shake detected",Toast.LENGTH_SHORT).show();
+    }
+    private void flashlighton(int c) throws CameraAccessException {
+        CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = null; // Usually front camera is at 0 position.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cameraId = camManager.getCameraIdList()[0];
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            camManager.setTorchMode(cameraId, true);
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void flashLightOff(int c){
+        CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        String cameraId = null; // Usually ront camera is at 0 position.
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                cameraId = camManager.getCameraIdList()[0];
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+
+        }
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                camManager.setTorchMode(cameraId, false);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,"Exception raised",Toast.LENGTH_SHORT)
+        }
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
